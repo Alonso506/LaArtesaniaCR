a4dbe6a6fba22cce589a211428e183fcd8662e28
Alonso506 <alonsoguzman.217@gmail.com>
2018-07-01 18:03:34 -1000

    metodos
    Crear, editar y vista administrativa

---
+++ la-artesania-cr/editar.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>La Artesanía CR</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	</head>
+	<body class="subpage">
+
+		
+<!-- Header -->
+			<header id="header">
+				<div class="logo"><a href="index.html">LaArtesaníaCR <span>SISTEMAS EXPERTOS</span></a></div>
+				<a href="#menu">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+                <ul class="links">
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="recomendaciones.html">Recomendaciones</a></li>
+                    <li><a href="categorias.html">Categorías</a></li>
+                    <li><a href="galeria.html">Galeria</a></li>
+                    <li><a href="sobreNosotros.html">Sobre Nosotros</a></li>
+                </ul>
+			</nav>
+			<!-- One -->
+			<section id="One" class="wrapper style3">
+				<div class="inner">
+					<header class="align-center">
+						<p>Login</p>
+						<h2>Ingrese</h2>
+					</header>
+				</div>
+			</section>
+<div class="container">
+  <h2>Editar</h2>
+  <form action="/action_page.php">
+    <div class="form-group">
+      <label for="nombre">Nombre:</label>
+      <input type="text" class="form-control" id="nombreArt" placeholder="Enter name" name="nombre">
+    </div>
+    <div class="form-group">
+      <label for="precio">Precio:</label>
+      <input type="number" class="form-control" id="precio" placeholder="Enter value" name="precio">
+    </div>
+    <div class="form-group">
+      <label for="largo">Largo:</label>
+      <input type="number" class="form-control" id="largo" placeholder="Enter size" name="largo">
+    </div>
+    <div class="form-group">
+      <label for="ancho">Ancho:</label>
+      <input type="number" class="form-control" id="ancho" placeholder="Enter size" name="ancho">
+    </div>
+    <div class="form-group">
+      <label for="tipo">Tipo:</label>
+      <input type="text" class="form-control" id="tipo" placeholder="Enter type" name="tipo">
+    </div>
+     <div class="form-group">
+      <label for="lugar">Lugar:</label>
+      <input type="text" class="form-control" id="lugar" placeholder="Enter place" name="lugar">
+    </div>
+    <div class="form-group form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-primary" ><a href="admin.php">Submit</a></button>
+  </form>
+</div>
+
+<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; Untitled. All rights reserved.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
